baca66bf42c67e911d5addfe35172d8204919b6a
seyar666 <93512293+seyar666@users.noreply.github.com>
2022-03-01 21:18:41 +0600

    my first commit

---
+++ heythere.java
@@ -0,0 +1,11 @@
+import util.java; 
+
+public class Seyar{
+
+	pubic static void main (String args []){
+
+	Scanner input = new Scanner(System.in); 
+
+	System.out.println("String" + input.nextLine()); 
+	}
+}
